021c21447090468edc274b3d78feb5eb11e4352f
pratikabhupendraagarwal <74354680+pratikabhupendraagarwal@users.noreply.github.com>
2020-11-30 14:00:05 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,20 @@
+class Chain {
+    constructor(bodyA,bodyB){
+    var options = {
+    bodyA: bodyA,
+    bodyB: bodyB,
+    lenght:10, stiffness:0.04
+    }
+     this.chain=Constraint.create(options);
+     World.add(world,this.chain)
+    }
+    
+    display(){
+    var pointA=this.chain.bodyA.position
+    var pointB= this.chain.bodyB.position
+    stroke("blue");
+    strokeWeight(3);
+    line (pointA.x,pointA.y,pointB.x,pointB.y)
+    }
+}
+
